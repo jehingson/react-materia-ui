3cbd9103f388cb04888d76699273624a612651db
jehingson <jehingson25@gmail.com>
2022-02-27 10:01:44 -0500

    [fix] first commit

---
+++ src/App/App.js
@@ -0,0 +1,59 @@
+import { createMuiTheme, CssBaseline, makeStyles, ThemeProvider } from '@material-ui/core';
+import React from 'react'
+import Header from '../components/Header';
+import SideMenu from '../components/SideMenu';
+import PeopleOutlineIcon from '@material-ui/icons/PeopleOutline';
+import PageHeader from '../components/PageHeader';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#333996",
+      light: "#3c44b126"
+    },
+    secondary: {
+      main: "#f83245",
+      light: "#f8324526"
+    },
+    background: {
+      default: "#f4f5fd"
+    }
+  },
+  overrides: {
+    MuiAppBar: {
+      root: {
+        transform: 'translatez(0)'
+      }
+    }
+  }
+})
+
+const useStyles = makeStyles({
+  appMain: {
+    paddingLeft: '320px',
+  }
+})
+
+
+
+function App() {
+  const classes = useStyles()
+
+
+  return (
+    <ThemeProvider theme={theme}>
+      <SideMenu />
+      <div className={classes.appMain}>
+        <Header />
+        <PageHeader 
+          title="Page Header"
+          subTitle="Page description"
+          icon={<PeopleOutlineIcon fontSize="large" />}
+        />
+      </div>
+      <CssBaseline />
+    </ThemeProvider>
+  );
+}
+
+export default App;
